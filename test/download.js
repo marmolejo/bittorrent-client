94c72d75a298a536d8d44ef37565dd1b831b2501
Feross Aboukhadijeh <feross@feross.org>
2014-08-16 23:33:47 -0700

    add failing test for tracker only without metadata

--- test/download.js
+++ test/download.js
@@ -135,6 +135,87 @@ test('Simple download using HTTP tracker', function (t) {
   downloadTrackerTest(t, 'http')
 })
 
+test('Simple download using a tracker (only) via a magnet uri', function (t) {
+  t.plan(8)
+
+  var trackerStartCount = 0
+
+  var magnetUri
+  auto({
+    tracker: function (cb) {
+      var tracker = new TrackerServer('udp')
+
+      tracker.on('error', function (err) {
+        t.fail(err)
+      })
+
+      tracker.on('start', function () {
+        trackerStartCount += 1
+      })
+
+      tracker.listen(function (port) {
+        var announceUrl = 'udp://127.0.0.1:' + port
+        magnetUri = 'magnet:?xt=urn:btih:' + leavesParsed.infoHash + '&tr=' + encodeURIComponent(announceUrl)
+        cb(null, tracker)
+      })
+    },
+
+    client1: ['tracker', function (cb) {
+      var client1 = new BitTorrentClient({ dht: false })
+      client1.on('error', function (err) { t.fail(err) })
+
+      client1.add(magnetUri)
+
+      client1.on('torrent', function (torrent) {
+        // torrent metadata has been fetched -- sanity check it
+        t.equal(torrent.name, 'Leaves of Grass by Walt Whitman.epub')
+
+        var names = [
+          'Leaves of Grass by Walt Whitman.epub'
+        ]
+
+        t.deepEqual(torrent.files.map(function (file) { return file.name }), names)
+
+        writeToStorage(torrent.storage, leavesFile, function (err) {
+          cb(err, client1)
+        })
+      })
+    }],
+
+    client2: ['client1', function (cb) {
+      var client2 = new BitTorrentClient({ dht: false })
+      client2.on('error', function (err) { t.fail(err) })
+
+      client2.add(magnetUri)
+
+      client2.on('torrent', function (torrent) {
+        torrent.files.forEach(function (file) {
+          file.createReadStream()
+        })
+
+        torrent.once('done', function () {
+          t.pass('client2 downloaded torrent from client1')
+          cb(null, client2)
+        })
+      })
+    }]
+
+  }, function (err, r) {
+    t.error(err)
+    t.equal(trackerStartCount, 2)
+
+    r.tracker.close(function () {
+      t.pass('tracker closed')
+    })
+    r.client1.destroy(function () {
+      t.pass('client1 destroyed')
+    })
+    r.client2.destroy(function () {
+      t.pass('client2 destroyed')
+    })
+  })
+})
+
 test('Simple download using DHT', function (t) {
   t.plan(7)
 
